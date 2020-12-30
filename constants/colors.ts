572e513f373c21339a747491ee06ad4b8161c78b
PranavGPR <pranav.gnanasekar23@gmail.com>
2020-12-30 06:55:48 +0530

    styled components added

---
+++ constants/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  primarybg: "#061018",
+  secondarybg: "#2F2F2F",
+  primary: "#007ACC",
+  textcolor: "#EBEBEB",
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from "styled-components";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel='icon'
+          href='favicon.png'
+          sizes='16x16 28x28 32x32'
+          type='image/png'
+        />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Dancing+Script&family=Fira+Code:wght@400;500&family=Oxygen:wght@300;400;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: Oxygen, Helvetica Neue, sans-serif;
+  scroll-behavior: smooth;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
