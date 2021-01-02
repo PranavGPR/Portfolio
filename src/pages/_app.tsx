86fbb5cde29b4701677a59a82c8bdbc541036c91
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-02 19:36:23 +0530

    bug fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <meta name='author' content='Pranav' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta http-equiv='X-UA-Compatible' content='ie=edge' />
+        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
 
         {/* Favicon */}
         <link
