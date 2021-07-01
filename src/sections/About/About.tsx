64529a5001d8f8d8c5008e95905a0b57b4e0dc83
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-07-01 10:24:40 +0530

    HTML tags replaced

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -26,7 +26,7 @@ export default function About() {
     <>
       <section className={styles.about} id='about'>
         <div className={styles.about__container}>
-          <div style={{ marginBottom: "5rem", width: "100%" }}>
+          <section style={{ marginBottom: "5rem", width: "100%" }}>
             <Fade bottom cascade>
               <Typography variant='h3'>About Me</Typography>
             </Fade>
@@ -67,9 +67,9 @@ export default function About() {
                 </figure>
               </Fade>
             </div>
-          </div>
+          </section>
 
-          <div className={styles.education}>
+          <section className={styles.education}>
             <Fade bottom cascade>
               <Typography variant='h4' style={{ marginBottom: "3rem" }}>
                 Education
@@ -82,9 +82,9 @@ export default function About() {
                 </Accordion>
               );
             })}
-          </div>
+          </section>
 
-          <div className={styles.experience}>
+          <section className={styles.experience}>
             <Fade bottom cascade>
               <Typography variant='h4' style={{ marginBottom: "3rem" }}>
                 Experience
@@ -105,157 +105,155 @@ export default function About() {
                 );
               }
             )}
-          </div>
+          </section>
         </div>
       </section>
       <section id='skills' className={styles.skills}>
         <Fade bottom cascade>
-          <div>
-            <Typography variant='h3' style={{ marginBottom: "3rem" }}>
-              Skills
-            </Typography>
-            <div className={styles.skills__icons}>
-              <span className='icon icon-html5'>
-                <span className='tooltip'>HTML</span>
-              </span>
+          <Typography variant='h3' style={{ marginBottom: "3rem" }}>
+            Skills
+          </Typography>
+          <div className={styles.skills__icons}>
+            <span className='icon icon-html5'>
+              <span className='tooltip'>HTML</span>
+            </span>
 
-              <span className='icon icon-css3'>
-                <span className='tooltip'>CSS</span>
-              </span>
+            <span className='icon icon-css3'>
+              <span className='tooltip'>CSS</span>
+            </span>
 
-              <span className='icon icon-sass'>
-                <span className='tooltip'>Sass</span>
-              </span>
+            <span className='icon icon-sass'>
+              <span className='tooltip'>Sass</span>
+            </span>
 
-              <span className='icon icon-styled-components'>
-                <span className='tooltip'>Styled Components</span>
-              </span>
+            <span className='icon icon-styled-components'>
+              <span className='tooltip'>Styled Components</span>
+            </span>
 
-              <span className='icon icon-javascript'>
-                <span className='tooltip'>Javascript</span>
-              </span>
+            <span className='icon icon-javascript'>
+              <span className='tooltip'>Javascript</span>
+            </span>
 
-              <span className='icon icon-typescript'>
-                <span className='tooltip'>TypeScript</span>
-              </span>
+            <span className='icon icon-typescript'>
+              <span className='tooltip'>TypeScript</span>
+            </span>
 
-              <span className='icon icon-react'>
-                <span className='tooltip'>React</span>
-              </span>
+            <span className='icon icon-react'>
+              <span className='tooltip'>React</span>
+            </span>
 
-              <span className='icon icon-redux'>
-                <span className='tooltip'>Redux</span>
-              </span>
+            <span className='icon icon-redux'>
+              <span className='tooltip'>Redux</span>
+            </span>
 
-              <span className='icon icon-next-dot-js'>
-                <span className='tooltip'>NextJS</span>
-              </span>
+            <span className='icon icon-next-dot-js'>
+              <span className='tooltip'>NextJS</span>
+            </span>
 
-              <span className='icon icon-apollo'>
-                <span className='tooltip'>Apollo</span>
-              </span>
+            <span className='icon icon-apollo'>
+              <span className='tooltip'>Apollo</span>
+            </span>
 
-              <span className='icon icon-graphql'>
-                <span className='tooltip'>GraphQL</span>
-              </span>
+            <span className='icon icon-graphql'>
+              <span className='tooltip'>GraphQL</span>
+            </span>
 
-              <span className='icon icon-node-dot-js'>
-                <span className='tooltip'>NodeJS</span>
-              </span>
+            <span className='icon icon-node-dot-js'>
+              <span className='tooltip'>NodeJS</span>
+            </span>
 
-              <span className='icon icon-express'>
-                <span className='tooltip'>Express</span>
-              </span>
+            <span className='icon icon-express'>
+              <span className='tooltip'>Express</span>
+            </span>
 
-              <span className='icon icon-jest'>
-                <span className='tooltip'>Jest</span>
-              </span>
+            <span className='icon icon-jest'>
+              <span className='tooltip'>Jest</span>
+            </span>
 
-              <span className='icon icon-mongodb'>
-                <span className='tooltip'>MongoDB</span>
-              </span>
+            <span className='icon icon-mongodb'>
+              <span className='tooltip'>MongoDB</span>
+            </span>
 
-              <span className='icon icon-firebase'>
-                <span className='tooltip'>Firebase</span>
-              </span>
+            <span className='icon icon-firebase'>
+              <span className='tooltip'>Firebase</span>
+            </span>
 
-              <span className='icon icon-postgresql'>
-                <span className='tooltip'>PostgreSQL</span>
-              </span>
+            <span className='icon icon-postgresql'>
+              <span className='tooltip'>PostgreSQL</span>
+            </span>
 
-              <span className='icon icon-mysql'>
-                <span className='tooltip'>MySQL</span>
-              </span>
+            <span className='icon icon-mysql'>
+              <span className='tooltip'>MySQL</span>
+            </span>
 
-              <span className='icon icon-sequelize'>
-                <span className='path1'></span>
-                <span className='path2'></span>
-                <span className='path3'></span>
-                <span className='path4'></span>
-                <span className='path5'></span>
-                <span className='path6'></span>
-                <span className='path7'></span>
-                <span className='path8'></span>
-                <span className='path9'></span>
-                <span className='path10'></span>
-                <span className='path11'></span>
-                <span className='path12'></span>
-                <span className='path13'></span>
-                <span className='path14'></span>
-                <span className='path15'></span>
-                <span className='path16'></span>
-                <span className='path17'></span>
-                <span className='path18'></span>
-                <span className='path19'></span>
-                <span className='path20'></span>
-                <span className='path21'></span>
-                <span className='path22'></span>
-                <span className='path23'></span>
-                <span className='path24'></span>
-                <span className='path25'></span>
-                <span className='path26'></span>
-                <span className='path27'></span>
-                <span className='tooltip'>Sequalize</span>
-              </span>
+            <span className='icon icon-sequelize'>
+              <span className='path1'></span>
+              <span className='path2'></span>
+              <span className='path3'></span>
+              <span className='path4'></span>
+              <span className='path5'></span>
+              <span className='path6'></span>
+              <span className='path7'></span>
+              <span className='path8'></span>
+              <span className='path9'></span>
+              <span className='path10'></span>
+              <span className='path11'></span>
+              <span className='path12'></span>
+              <span className='path13'></span>
+              <span className='path14'></span>
+              <span className='path15'></span>
+              <span className='path16'></span>
+              <span className='path17'></span>
+              <span className='path18'></span>
+              <span className='path19'></span>
+              <span className='path20'></span>
+              <span className='path21'></span>
+              <span className='path22'></span>
+              <span className='path23'></span>
+              <span className='path24'></span>
+              <span className='path25'></span>
+              <span className='path26'></span>
+              <span className='path27'></span>
+              <span className='tooltip'>Sequalize</span>
+            </span>
 
-              <span className='icon icon-docker'>
-                <span className='tooltip'>Docker</span>
-              </span>
+            <span className='icon icon-docker'>
+              <span className='tooltip'>Docker</span>
+            </span>
 
-              <span className='icon icon-c'>
-                <span className='tooltip'>C</span>
-              </span>
+            <span className='icon icon-c'>
+              <span className='tooltip'>C</span>
+            </span>
 
-              <span className='icon icon-cplusplus'>
-                <span className='tooltip'>C++</span>
-              </span>
+            <span className='icon icon-cplusplus'>
+              <span className='tooltip'>C++</span>
+            </span>
 
-              <span className='icon icon-java'>
-                <span className='path1'></span>
-                <span className='path2'></span>
-                <span className='path3'></span>
-                <span className='path4'></span>
-                <span className='path5'></span>
-                <span className='tooltip'>Java</span>
-              </span>
+            <span className='icon icon-java'>
+              <span className='path1'></span>
+              <span className='path2'></span>
+              <span className='path3'></span>
+              <span className='path4'></span>
+              <span className='path5'></span>
+              <span className='tooltip'>Java</span>
+            </span>
 
-              <span className='icon icon-python'>
-                <span className='tooltip'>Python</span>
-              </span>
+            <span className='icon icon-python'>
+              <span className='tooltip'>Python</span>
+            </span>
 
-              <span className='icon icon-git'>
-                <span className='tooltip'>Git</span>
-              </span>
+            <span className='icon icon-git'>
+              <span className='tooltip'>Git</span>
+            </span>
 
-              <span className='icon icon-figma'>
-                <span className='path1'></span>
-                <span className='path2'></span>
-                <span className='path3'></span>
-                <span className='path4'></span>
-                <span className='path5'></span>
-                <span className='tooltip'>Figma</span>
-              </span>
-            </div>
+            <span className='icon icon-figma'>
+              <span className='path1'></span>
+              <span className='path2'></span>
+              <span className='path3'></span>
+              <span className='path4'></span>
+              <span className='path5'></span>
+              <span className='tooltip'>Figma</span>
+            </span>
           </div>
         </Fade>
       </section>
