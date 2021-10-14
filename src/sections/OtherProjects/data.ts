7d55b5a8681c0414279b93b44e46172a0a3a2234
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-10-14 13:09:07 +0530

    Typo corrections

--- src/sections/OtherProjects/data.ts
+++ src/sections/OtherProjects/data.ts
@@ -1,52 +1,52 @@
 const extras = [
-  {
-    title: "Bonafide Generator API",
-    type: "Featured Project",
-    tech: "NodeJS Express Mongoose MongoDB",
-    repo: "https://github.com/PranavGPR/bonafide-gen-api",
-    link: "https://bonafide-generator.netlify.app",
-    description: "The API for Bonafide Generator.",
-  },
-  {
-    title: "Infinite Loop Club API",
-    type: "Contributed Project",
-    tech: "NodeJS Express Mongoose MongoDB",
-    repo: "https://github.com/Infinite-Loop-Club/club-site-api",
-    link: "https://infinite-loop-club.netlify.app/",
-    description: "The Official API for Infinite Loop Club website.",
-  },
-  {
-    title: "Nviktous",
-    type: "Contributed Project",
-    tech: "React Sass MaterialUI",
-    link: "https://nviktous.herokuapp.com/",
-    description: "An E-Commerce shopping site built with my colleagues.",
-  },
-  {
-    title: "AUBIT - Unofficial Site",
-    type: "Contributed Project",
-    tech: "React Sass",
-    link: "https://aubit.netlify.app",
-    description:
-      "The unofficial website of University College of Engineering, BIT Campus, Tiruchirappalli. The website is still under development and will be approved upon completion to be the official website. Built with React.",
-  },
-  {
-    title: "class-scheduler",
-    type: "Featured Project",
-    tech: "TypeScript JavaScript",
-    repo: "https://github.com/benxene/class-scheduler",
-    link: "https://www.npmjs.com/package/class-scheduler",
-    description: "A zero dependency class time-table manager.",
-  },
-  {
-    title: "Rahul's Dairy Farm",
-    type: "Featured Project",
-    tech: "HTML  CSS",
-    repo: "https://github.com/PranavGPR/Dairy-Farm-Project",
-    link: "https://rahuldairyfarm.netlify.app/",
-    description:
-      "A Landing Site designed and developed for Rahul's Dairy Farm.",
-  },
+	{
+		title: 'Bonafide Generator API',
+		type: 'Featured Project',
+		tech: 'NodeJS Express Mongoose MongoDB',
+		repo: 'https://github.com/PranavGPR/bonafide-gen-api',
+		link: 'https://bonafide-generator.netlify.app',
+		description: 'The API for Bonafide Generator.'
+	},
+	{
+		title: 'Nviktous',
+		type: 'Contributed Project',
+		tech: 'ReactJS Sass MaterialUI',
+		link: 'https://nviktous.herokuapp.com/',
+		description: 'An E-Commerce shopping site built with my colleagues.'
+	},
+	{
+		title: 'Infinite Loop Club API',
+		type: 'Contributed Project',
+		tech: 'NodeJS Express Mongoose MongoDB',
+		repo: 'https://github.com/Infinite-Loop-Club/club-site-api',
+		link: 'https://infinite-loop-club.netlify.app/',
+		description: 'The Official API for Infinite Loop Club website.'
+	},
+
+	{
+		title: 'AUBIT - Unofficial Site',
+		type: 'Contributed Project',
+		tech: 'ReactJS Sass',
+		link: 'https://aubit.netlify.app',
+		description:
+			'The unofficial website of University College of Engineering, BIT Campus, Tiruchirappalli. The website is still under development and will be approved upon completion to be the official website. Built with ReactJS.'
+	},
+	{
+		title: 'class-scheduler',
+		type: 'Featured Project',
+		tech: 'TypeScript JavaScript',
+		repo: 'https://github.com/benxene/class-scheduler',
+		link: 'https://www.npmjs.com/package/class-scheduler',
+		description: 'A zero dependency class time-table manager.'
+	},
+	{
+		title: "Rahul's Dairy Farm",
+		type: 'Featured Project',
+		tech: 'HTML  CSS',
+		repo: 'https://github.com/PranavGPR/Dairy-Farm-Project',
+		link: 'https://rahuldairyfarm.netlify.app/',
+		description: "A Landing Site designed and developed for Rahul's Dairy Farm."
+	}
 ];
 
 export default extras;
--- src/sections/Projects/data.ts
+++ src/sections/Projects/data.ts
@@ -1,55 +1,52 @@
 const data = [
-  {
-    image: "bonafide-generator.jpg",
-    type: "Featured Project",
-    title: "Bonafide Generator",
-    stack: ["React", "Styled Components", "@react-pdf/renderer"],
-    link: "https://bonafide-generator.netlify.app/",
-    repo: "https://github.com/sank2000/bonafide-generator",
-    description:
-      "A MERN Stack application for a student to obtain an online bonafide certificate.",
-  },
-  {
-    image: "infinite-loop-club.jpg",
-    type: "Featured Project",
-    title: "Infinite Loop Club Website",
-    stack: ["React", "Styled Components"],
-    link: "https://infinite-loop-club.netlify.app/",
-    repo: "https://github.com/Infinite-Loop-Club/club-site",
-    description:
-      "The official site of Infinite loop club of Anna University, Trichy.",
-  },
-  {
-    image: "Savory-api.jpg",
-    type: "Featured Project",
-    title: "Savory API - A food ordering API",
-    stack: ["Node.js", "Express", "MySQL", "Swagger"],
-    link: "https://savory-api.herokuapp.com/",
-    repo: "https://github.com/PranavGPR/savory-api",
-    description:
-      "The REST API backend for Savory, a food ordering app API, built on Node.js and Express with MySQL as the Database. Swagger UI is used to create documentation of the routes for this REST API.",
-  },
-  {
-    image: "fullycart.jpg",
-    type: "Featured Project",
-    title: "FullyCart",
-    stack: ["React", "Redux", "Node.js", "Express", "MongoDB"],
-    link: "https://fullycart.herokuapp.com/",
-    repo: "https://github.com/PranavGPR/FullyCart",
-    description:
-      "An E-Commerce Clothes shopping site built on React and Redux for FrontEnd, Express for Server and MongoDB for Database.",
-  },
-  {
-    image: "7days50projects.jpg",
-    type: "Collaborated Project",
-    title: "7Days - 50Projects",
-    stack: ["NextJS", "React", "Styled Components", "TypeScript"],
-    link: "https://7days50projects.vercel.app/",
-    repo: "https://github.com/benxene/7days-50projects",
-    description:
-      "This is a derivative of 50days50projects which is a course consisting of 50 mini projects produced by Brad Traversy and Florin Pop. Me and my colleages designed and built it on Next JS, Styled Components and TypeScript.",
-  },
-  
+	{
+		image: 'bonafide-generator.jpg',
+		type: 'Featured Project',
+		title: 'Bonafide Generator',
+		stack: ['ReactJS', 'Styled Components', '@react-pdf/renderer'],
+		link: 'https://bonafide-generator.netlify.app/',
+		repo: 'https://github.com/sank2000/bonafide-generator',
+		description: 'A MERN Stack application for a student to obtain an online bonafide certificate.'
+	},
+	{
+		image: 'infinite-loop-club.jpg',
+		type: 'Featured Project',
+		title: 'Infinite Loop Club Website',
+		stack: ['ReactJS', 'Styled Components'],
+		link: 'https://infinite-loop-club.netlify.app/',
+		repo: 'https://github.com/Infinite-Loop-Club/club-site',
+		description: 'The official site of Infinite loop club of Anna University, Trichy.'
+	},
+	{
+		image: 'Savory-api.jpg',
+		type: 'Featured Project',
+		title: 'Savory API - A food ordering API',
+		stack: ['Node.js', 'Express', 'MySQL', 'Swagger'],
+		link: 'https://savory-api.herokuapp.com/',
+		repo: 'https://github.com/PranavGPR/savory-api',
+		description:
+			'The REST API backend for Savory, a food ordering app API, built on Node.js and Express with MySQL as the Database. Swagger UI is used to create documentation of the routes for this REST API.'
+	},
+	{
+		image: 'fullycart.jpg',
+		type: 'Featured Project',
+		title: 'FullyCart',
+		stack: ['ReactJS', 'Redux', 'Node.js', 'Express', 'MongoDB'],
+		link: 'https://fullycart.herokuapp.com/',
+		repo: 'https://github.com/PranavGPR/FullyCart',
+		description:
+			'An E-Commerce Clothes shopping site built on ReactJS and Redux for FrontEnd, Express for Server and MongoDB for Database.'
+	},
+	{
+		image: '7days50projects.jpg',
+		type: 'Collaborated Project',
+		title: '7Days - 50Projects',
+		stack: ['NextJS', 'ReactJS', 'Styled Components', 'TypeScript'],
+		link: 'https://7days50projects.vercel.app/',
+		repo: 'https://github.com/benxene/7days-50projects',
+		description:
+			'This is a derivative of 50days50projects which is a course consisting of 50 mini projects produced by Brad Traversy and Florin Pop. Me and my colleages designed and built it on Next JS, Styled Components and TypeScript.'
+	}
 ];
 
 export default data;
