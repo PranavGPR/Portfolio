9f6ac0dbff98a3a10076cb695e1a2e362189d50d
PranavGPR <pranav.gnanasekar23@gmail.com>
2025-06-10 21:22:26 +0530

    import the font from html instead of scss

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Fira+Code&family=Oxygen:wght@300;400;700&display=swap'
+						rel='stylesheet'
+					/>
 					<link rel='manifest' href='/manifest.json' />
 					<link rel='apple-touch-icon' href='/apple-touch-icon.png'></link>
 					<meta name='theme-color' content='#fff' />
